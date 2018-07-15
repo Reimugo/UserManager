1406530636f899263111e5b27b6348b09ad3d2d0
songshu <xhskedu@qq.com>
2018-07-15 15:36:33 +0800

    modify Pack class

--- src/Pack.java
+++ src/Pack.java
@@ -1,3 +1,5 @@
+import domain.User;
+
 import java.io.Serializable;
 
 /**
@@ -5,6 +7,7 @@ import java.io.Serializable;
  *
  * packType - 表示此次发送的请求类型，如（开课/选课/登录/注册）等，详情请看枚举类型 PackType
  * result - 服务器返回此对象时，可用此字段表示是否成功
+ * user - 操作的用户
  * data - 附带数据，如User对象、Course对象以及Java集合等，若无附加数据则传递null即可
  *
  */
@@ -12,6 +15,7 @@ import java.io.Serializable;
 public class Pack implements Serializable {
     private PackType packType;
     private boolean result;
+    private User operator;
     private Object data;
 
     public Pack(PackType packType, boolean result, Object data) {
@@ -34,6 +38,14 @@ public class Pack implements Serializable {
 
     public void setResult(boolean result) {
         this.result = result;
+    }
+
+    public User getOperator() {
+        return operator;
+    }
+
+    public void setOperator(User operator) {
+        this.operator = operator;
     }
 
     public Object getData() {
